79e7c05993c83d1e24e41ae48059f6d6231fc639
Chris Barry <chris.barry@makemoredigital.com>
2023-07-27 18:12:03 +0100

    added back main server bits

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var prerender = require('./lib');
 
 var server = prerender();
 
-// server.use(prerender.sendPrerenderHeader());
-// server.use(prerender.browserForceRestart());
-// // server.use(prerender.blockResources());
-// server.use(prerender.addMetaTags());
-// server.use(prerender.removeScriptTags());
-// server.use(prerender.httpHeaders());
+server.use(prerender.sendPrerenderHeader());
+server.use(prerender.browserForceRestart());
+// server.use(prerender.blockResources());
+server.use(prerender.addMetaTags());
+server.use(prerender.removeScriptTags());
+server.use(prerender.httpHeaders());
 
-// server.start();
+server.start();
